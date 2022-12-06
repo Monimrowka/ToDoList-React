67242affe4a297026335ebc5cd4a7253fe1aa98f
Neelima Rapeti <neelimarapeti@gmail.com>
2022-12-06 21:48:18 +0100

    basic color

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -5,10 +5,12 @@ import Todo from './Component/Todo';
 let doList = [
   {
     text: "Learn HTML",
+    
     uniqueNo: 1
   },
   {
     text: "Learn CSS",
+   
     uniqueNo: 2
   },
   {
@@ -18,34 +20,35 @@ let doList = [
 ];
 
 
-function App() {
+ function App() {
+  const addfn =() =>
+  {  
+
+  }
   return (
     <>
      <div className="dos-bg-container">
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1 className="dos-heading">Main</h1>
+          <h1 className="dos-heading">To-do-list</h1>
           <h1 className="create-task-heading">
             Create <span className="create-task-heading-subpart">Task</span>
           </h1>
           <input type="text" id="doUserInput" className="todo-user-input" />
-          <button className="add-do-button">Add</button>
+          <button className="add-do-button" onClick={addfn}>Add</button>
           <h1 className="do-items-heading">
             My <span className="do-items-heading-subpart">Tasks</span>
           </h1>
           <ul className="do-items-container" id="doItemsContainer">
           {doList.map((eachItem) => (
-           <Todo Details={eachItem} />
+           <Todo textcontent={eachItem} />
           
           ))}
           
           
           </ul>
-        </div>
+        
       </div>
-    </div>
-  </div>
+    
+  
 </>
   );
 }
--- to-do-list-react/src/Component/Todo.js
+++ to-do-list-react/src/Component/Todo.js
@@ -1,32 +1,29 @@
 import React from 'react';
 
+
 const Todo = props =>
-{
-    const {Details} = props
-    const {text} = Details
+{ console.log(props)
+   const {textcontent} =props
+    const {text} = textcontent
 
 return(
    
-    <div>
-    <li className="todo-item-container d-flex flex-column"></li>
-    <input type="checkbox" id="checkboxInput" className="checkbox-input" />
-        
-        <div className="d-flex flex-row label-container">
-          <label for="checkboxInput" className="checkbox-label">{text}
     
-          </label>
-          
-        </div>
-
-    </div>
+    <li className="todo-item-container d-flex flex-column">
+    <input type="checkbox" className="checkbox-input" />
         
-   
-);
+        <div className = "d-flex flex-row label-container">
+        
+         
+          <label for="checkboxInput" className="checkbox-label">{text}
+          </label>
+          <button className="deletebtn">Delete</button>
+            
+          </div>
+        </li>
+        
+
+    
+        );
 }
-
-
-
-
-
-
 export default Todo;
