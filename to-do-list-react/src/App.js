feec5e7856b1c560caf53c07f2cec1a265ea4534
Neelima Rapeti <neelimarapeti@gmail.com>
2022-12-06 19:08:52 +0100

    objectarray added with map

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -1,35 +1,52 @@
 import './App.css';
 import Todo from './Component/Todo';
 
+
+let doList = [
+  {
+    text: "Learn HTML",
+    uniqueNo: 1
+  },
+  {
+    text: "Learn CSS",
+    uniqueNo: 2
+  },
+  {
+    text: "Learn JavaScript",
+    uniqueNo: 3
+  }
+];
+
+
 function App() {
   return (
     <>
-     <div className="todos-bg-container">
+     <div className="dos-bg-container">
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h1 className="todos-heading">Todos</h1>
+          <h1 className="dos-heading">Main</h1>
           <h1 className="create-task-heading">
             Create <span className="create-task-heading-subpart">Task</span>
           </h1>
-          <input type="text" id="todoUserInput" className="todo-user-input" />
-          <button className="add-todo-button">Add</button>
-          <h1 className="todo-items-heading">
-            My <span className="todo-items-heading-subpart">Tasks</span>
+          <input type="text" id="doUserInput" className="todo-user-input" />
+          <button className="add-do-button">Add</button>
+          <h1 className="do-items-heading">
+            My <span className="do-items-heading-subpart">Tasks</span>
           </h1>
-          <ul className="todo-items-container" id="todoItemsContainer">
-
-          <Todo />
+          <ul className="do-items-container" id="doItemsContainer">
+          {doList.map((eachItem) => (
+           <Todo Details={eachItem} />
+          
+          ))}
+          
           
           </ul>
         </div>
       </div>
     </div>
   </div>
-    </>
-    
-
-   
+</>
   );
 }
 
--- to-do-list-react/src/Component/Todo.js
+++ to-do-list-react/src/Component/Todo.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import List from './List'
-function Todo()
+
+const Todo = props =>
 {
+    const {Details} = props
+    const {text} = Details
 
 return(
    
     <div>
     <li className="todo-item-container d-flex flex-column"></li>
-    <List />
+    <input type="checkbox" id="checkboxInput" className="checkbox-input" />
+        
+        <div className="d-flex flex-row label-container">
+          <label for="checkboxInput" className="checkbox-label">{text}
+    
+          </label>
+          
+        </div>
 
     </div>
         
